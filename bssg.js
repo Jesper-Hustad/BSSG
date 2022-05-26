8b32356d5f07cce4636ec90625a0f798918507ff
Jesper Hustad <jesterbe3@gmail.com>
2022-05-26 14:15:17 +0100

    mvp

--- bssg.js
+++ bssg.js
@@ -2,16 +2,14 @@ const fs = require('fs-extra');
 const path = require('path');
 const glob = require('util').promisify(require("glob"));
 const showdown = require('showdown')
-var converter = new showdown.Converter({metadata: true})
+const showdownHighlight = require("showdown-highlight")
+let converter = new showdown.Converter({metadata: true, tables: true, extensions: [showdownHighlight({pre: true})]})
 
-// ------------------------------------
-// Set the html template variable names here
-// ------------------------------------
-const SETTINGS = {
-    pages : 'pages',
-    content : 'content',
-    date: 'date'
-}
+// sections in the metadata that get added to template
+const METADATA_SECTIONS = ['title', 'date']
+
+const INDEX_LIST_VAR = 'pages'
+const TEMPLATE_CONTENT_VAR = 'content'
 
 class HtmlTemplate {
     constructor(html) {this.html = html}
@@ -24,9 +22,10 @@ const template = HtmlTemplate.fromFile("layout/template.html")
 
 async function main(){
 
-    const public = path.join(__dirname, '/public')
-    await fs.remove(public); 
+    const public = path.join(__dirname, '/public'), source = path.join(public, '/_source')
+    await fs.emptyDir('./output', {recursive: true, force: true});
     await fs.copy(path.join(__dirname, '/src'), public)
+    await fs.copy(path.join(__dirname, '/layout'), source, {filter: i=>i.split('.').pop()!='html'})
 
     const pages = (await glob('public/**/*'))
         .filter(f => !f.includes("node_modules/"))
@@ -35,14 +34,26 @@ async function main(){
 
     pages.forEach(i => makeTemplate(...i))
         
-    fs.writeFile("./public/index.html", index.add(SETTINGS.pages, getPageList(pages)).html,()=>{})
+    fs.writeFile("./public/index.html", index.add(INDEX_LIST_VAR, getPageList(pages)).html,()=>{})
 }
 
 
-function makeTemplate(html, title, newPath, date){
-    let result = template.add(SETTINGS.content, html).add(SETTINGS.date, date).html
+async function makeTemplate(html, title, newPath, date, metadata){
+    // 
+    let result = template.add(TEMPLATE_CONTENT_VAR, html)
+    METADATA_SECTIONS.forEach(i => result = result.add(i, getMetadata(metadata, i)))
+    result = result.html
+
+    
+
+    const relative = (source, location) => path.relative(path.dirname(location),path.join("./public/_source",source))
+
+    const imports = (await glob('layout/**/*')).map(i => i.replace("layout/","")).filter(i => !['html','htm'].includes(i.split('.').pop()))
+    imports.forEach(i => result = result.replace(new RegExp(`(?<=href=")\/?${i}\/?(?=")`,'g'), relative(i, newPath)))
+
     fs.writeFile(newPath, result,()=>{})
 }
+
 
 const getPageList = (pages) => {
     const toHtmlLink = (file) => file.substr(0,file.lastIndexOf(".")).replace('public'+path.sep, '')
@@ -56,9 +67,12 @@ const parseMarkdownFile = filename => {
     const title = getMetadata(metadata, 'title')
     const newPath = path.join(path.dirname(filename), (title.toLowerCase().replaceAll(" ","-")+".html"))
     const date = getMetadata(metadata, 'date')
-    return [html, title, newPath, date]
+    return [html, title, newPath, date, metadata]
 }
 
 const getMetadata = (data,key) => data.filter(i=>i.toLocaleLowerCase().substring(0,key.length)==key).map(i=>i.substring(i.indexOf(":")+1).trim())[0]
 
 main()
+
+
+
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-// const path = require('path');
+const path = require('path');
 // var DomParser = require('dom-parser');
-// var fs = require('fs-extra');
+var fs = require('fs-extra');
 // var parser = new DomParser();
+const glob = require('util').promisify(require("glob"));
 
 // const dom = parser.parseFromString(fs.readFileSync("layout/template.html").toString())
 
@@ -13,12 +14,11 @@
 // filter out includes
 
 
-// path.relative(path.dirname())
 
 let html = `<!DOCTYPE html>
 <html lang="en">
 <body>
-    <article class="markdown-body">
+    <style href="style.css">
     <h1>Home page</h1>
     <p>Pages:</p>
     {{ test }}
@@ -28,16 +28,71 @@ let html = `<!DOCTYPE html>
     </article>
 </body>`
 
-html = '{{ test }}    asdads'
+// html = '{{ test }}    asdads'
 
-console.log(html)
-console.log('-----------------------------------------------')
+// console.log(html)
+// console.log('-----------------------------------------------')
 
-html.replaceAll('{{ test }}',"---------NEW CONTENT----------")
+// html.replaceAll('{{ test }}',"---------NEW CONTENT----------")
 
-console.log(html)
+// console.log(html)
+
+// (?<=href=")[^>]+(?=")
 
 
-const p = '{{ test }}   {{ *test *}}The quick brown {{test     }} jumps over the lazy dog. If the dog reacted, was it really lazy?';
+// const p = '{{ test }}   {{ *test *}}The quick brown {{test     }} jumps over the lazy dog. If the dog reacted, was it really lazy?';
 
-console.log(p.replace(new RegExp(`{{ *test *}}`,'g'), "replaced text"));
+// console.log(p.replace(new RegExp(`{{ *test *}}`,'g'), "replaced text"));
+
+
+
+async function main(){
+
+    // const html = fs.readFileSync("layout/template.html").toString()
+    const newPath = "./public/folder/article.html"
+
+    const html_href = "./style.css"
+    const file_location = "./public/folder/article.html"
+
+    const a = path.relative(path.dirname(file_location),path.join("./public/_source",html_href))
+
+    console.log(html)
+
+    const relative = (source, location) => path.relative(path.dirname(location),path.join("./public/_source",source))
+
+
+
+    
+
+    // console.log(b.map(i => i.replace("layout/","")).filter(i => !['html','htm'].includes(i.split('.').pop())))
+    const imports = (await glob('layout/**/*')).map(i => i.replace("layout/","")).filter(i => !['html','htm'].includes(i.split('.').pop()))
+    imports.forEach(i => html = html.replace(new RegExp(`(?<=href=")\/?${i}\/?(?=")`,'g'), relative(i, newPath)))
+
+        // 
+
+    console.log(html)
+
+
+
+    // console.log(p)
+}
+
+// (?<=href=")\/?posts\/?(?=")
+
+main()
+
+// function makeTemplate(html, title, newPath, date){
+//     let result = template.add(SETTINGS.content, html).add(SETTINGS.date, date).html
+//     // for every js or css file in layout
+//         // replace all references in template with relative one
+//     result.replaceAll(js_import, relative_js_import)
+//     // replace every css and js link with relative
+//     fs.writeFile(newPath, result,()=>{})
+// }
+
+
+
+
+
+
+
